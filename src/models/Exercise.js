1bda291c767de96c4e6afa6ed3c6ad0dde5eddf5
Rendi Hidayah <12450114867@students.uin-suska.ac.id>
2026-05-15 20:12:36 +0700

    Update model Exercise: ganti bodyPart ke muscle dan tambah field baru

--- src/models/Exercise.js
+++ src/models/Exercise.js
@@ -1,12 +1,31 @@
 const mongoose = require('mongoose');
 
 const exerciseSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    bodyPart: { type: String, required: true }, // Contoh: Chest, Back
-    equipment: { type: String, required: true }, // Contoh: Dumbbell, Machine
-    target: { type: String }, // Otot spesifik: Triceps, Lats
-    gifUrl: { type: String }, // Link gambar/animasi gerakan
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // Siapa yang buat (opsional)
-});
+    name: {
+        type: String,
+        required: true
+    },
+    muscle: {
+        type: String,
+        required: true
+    }, // Contoh: Chest, Back, Legs
+    equipment: {
+        type: String,
+        required: true
+    }, // Contoh: Barbell, Dumbbell, Bodyweight
+    instructions: {
+        type: String
+    }, // Penjelasan cara melakukan gerakan
+    videoUrl: {
+        type: String
+    }, // Link video (YouTube/dll)
+    image: {
+        type: String
+    }, // Nama file atau link gambar
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    } // Opsional: Untuk menandai siapa yang menambahkan
+}, { timestamps: true });
 
 module.exports = mongoose.model('Exercise', exerciseSchema);
